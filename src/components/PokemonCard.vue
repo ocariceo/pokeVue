ed03bd9b6b6f16596ca3547e73808c7b4e67b510
ocariceo <ocariceor@yahoo.es>
2025-05-20 15:30:37 -0400

    Add files via upload

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pokemon-card">
+    <h2>{{ pokemon.name }}</h2>
+    <img :src="pokemon.image" :alt="pokemon.name" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokemonCard',
+  props: {
+    pokemon: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
